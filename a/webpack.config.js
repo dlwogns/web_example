e16fca3b3f7b5141e6560b15291ac70c1632df8c
dlwogns <ldyjka@gmail.com>
2022-06-06 04:23:44 +0000

    first step to react - express

---
+++ a/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+ 
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname + "/build")
+  },
+  devServer: {
+    //contentBase: path.resolve("./build"),
+    //index: "index.html",
+    allowedHosts: "all",
+    port: 3000
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: "/node_modules",
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ]
+};
