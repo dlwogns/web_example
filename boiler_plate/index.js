c88cbcfb3ad99f6625f379397c638c17e5c238ce
dlwogns <ldyjka@gmail.com>
2022-06-12 09:21:17 +0000

    ~#15 without error fix

--- boiler_plate/index.js
+++ boiler_plate/index.js
@@ -7,6 +7,7 @@ const cookieParser = require('cookie-parser');
 const {User} = require("./models/User");
 
 const config = require('./config/key');
+const {auth} = require('./middleware/auth');
 
 //application/x-www-form-urlencoded -> 이런 형식의 데이터를 가져오기 위해
 app.use(bodyParser.urlencoded({extended: true}));
@@ -26,7 +27,7 @@ app.get('/', (req, res) => res.send('asdf'))
 //******************* 
 //register router
 //*******************
-app.post('/register', (req,res)=>{
+app.post('/api/users/register', (req,res)=>{
     //회원 가입 할때 필요한 정보들을 client에서 가져오면
     //그것들을 데이터베이스에 넣어준다.
     const user = new User(req.body) 
@@ -40,8 +41,10 @@ app.post('/register', (req,res)=>{
         })
     }) //from mongodb
 })
-
-app.post('/login', (req, res) =>{
+//*******************
+//login router
+//*******************
+app.post('/api/users/login', (req, res) =>{
     
     //요청된 이메일을 데이터베이스에서 있는지 확인.
     User.findOne({ email: req.body.email}, (err,user)=>{
@@ -69,10 +72,41 @@ app.post('/login', (req, res) =>{
         })
     
     })
-    
-
 })
 
+
+//******************* 
+//auth router
+//******************* 
+
+app.get('/api/users/auth',auth,(req, res)=>{
+    //여기까지 미들웨어(auth) 를 통과해 왔다는 것은
+    //authentication이 true라는 것.
+    res.statue(200).json({
+        _id : req.user._id,
+        isAdmin: req.user.role === 0? false:true,
+        isAuth:true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname : req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
+})
+
+//******************** 
+//logout router
+//********************
+app.get('/api/users/logout',auth, (req, res)=>{
+    User.findOneAndUpdate({_id: req.user._id},
+        {token: ""},
+        (err, user) =>{
+            if(err) return res.json({success: false, err});
+            return res.status(200).send({
+                success:true
+            })
+        })
+})
 
 app.listen(port, ()=> console.log('Example app listening on port ${port} !'))
 //app이 listen한다면, terminal에 해당 로그를 출력.
