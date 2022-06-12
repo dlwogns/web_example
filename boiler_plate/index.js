b1ebb955fb26420a375310c12b7c9cd77da7d1aa
dlwogns <ldyjka@gmail.com>
2022-06-12 07:31:44 +0000

    ~login #12

--- boiler_plate/index.js
+++ boiler_plate/index.js
@@ -3,15 +3,18 @@ const app = express()
 const port = 3001
 
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const {User} = require("./models/User");
 
-const config = require('./config/key')
+const config = require('./config/key');
 
 //application/x-www-form-urlencoded -> 이런 형식의 데이터를 가져오기 위해
 app.use(bodyParser.urlencoded({extended: true}));
 
 //application/json -> json형식의 데이터를 가져오기 위해
 app.use(bodyParser.json());
+
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 //mongodb를 편하게 쓰기 위한 api
@@ -20,19 +23,54 @@ mongoose.connect(config.mongoURI).then(()=>console.log('MongoDB Connected...')).
 
 app.get('/', (req, res) => res.send('asdf'))
 //route directory에 핼로 월드가 출력되게 함.
-
+//******************* 
+//register router
+//*******************
 app.post('/register', (req,res)=>{
     //회원 가입 할때 필요한 정보들을 client에서 가져오면
     //그것들을 데이터베이스에 넣어준다.
     const user = new User(req.body) 
     //req.body는 json 형식으로 들어오는데,
     //body-parser 덕분에 받을 수 있는 것.
-    user.save((err,doc)=>{
+
+    user.save((err,userInfo)=>{
         if(err) return res.json({success:false, err})
         return res.status(200).json({
             success:true
         })
     }) //from mongodb
+})
+
+app.post('/login', (req, res) =>{
+    
+    //요청된 이메일을 데이터베이스에서 있는지 확인.
+    User.findOne({ email: req.body.email}, (err,user)=>{
+        if(!user){
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+        //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는지 확인.
+        user.comparePassword(req.body.password , (err, isMatch)=>{
+            if(!isMatch)
+                return res.json({loginSuccess: false, message:"비밀번호가 틀렸습니다"})
+            
+            //비밀번호까지 맞다면 토큰을 생성.
+            user.generateToken((err,user) => {
+                if(err) return res.status(400).send(err);
+                
+                //토큰을 저장한다. 쿠키, 로컬 스토리지
+                res.cookie("x_auth",user.token)
+                .status(200)
+                .json({loginSuccess:true, userId: user._id})
+            })
+        
+        })
+    
+    })
+    
+
 })
 
 
--- boiler_plate/models/User.js
+++ boiler_plate/models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name : {
@@ -31,6 +34,61 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save', function(next){
+    var user = this;
+    // 다른 정보들이 바뀌어도 자동으로 전처리로 넘어오기 때문ㅇ
+    //password가 다시 암호화 될 수 있음.
+    //그러므로 isModified password 옵션을 걸어줘
+    //password가 변경될 경우에만 암호화를 다시 걸어주도록 함.
+   if(user.isModified('password')){
+
+    //비밀번호 암호화
+    //salt를 사용해서 encrypt하는 기능.
+    bcrypt.genSalt(saltRounds, function(err, salt){
+        if(err) return next(err)
+
+        bcrypt.hash(user.password, salt, function(err, hash){
+            if(err) return next(err) //모든 결과값을 받는 코드를 만들때는 err를 catch해주어야 한다.
+            user.password = hash
+            next()
+        })
+    })
+    //여기는 register route에서 save하기 전에 전처리를 하기 위한 코드이다.
+    //next함수를 써주면 index.js에서 save하는 부분으로 넘어간다.
+    } else{
+        //다른정보를 바꿀때는 넘어가야 하므로
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    //plainpassword 1234567 && encrypted 
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err),
+        cb(null, isMatch)
+    })
+}
+
+
+userSchema.methods.generateToken = function(cb){
+    //jsonwebtoken을 이용해 토큰 생성.
+    var user = this;
+    
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+    user.token = token
+    user.save(function(err,user){
+        if(err) return cb(err)
+        cb(null, user)
+    })
+
+
+
+}
+
+
+
 
 const User = mongoose.model('User', userSchema)
 
